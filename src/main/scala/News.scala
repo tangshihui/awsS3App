28ecf63d4fe532fd7c51dae110f958fff99bdf7b
ShihuiTang <tsh_index@163.com>
2019-03-28 13:57:34 +0800

    add article

--- src/main/scala/News.scala
+++ src/main/scala/News.scala
@@ -1,3 +1,15 @@
 object News {
 
+  class Article(title: String, content: String) {
+
+    override def toString: String = {
+      s"title:${title} content:${content}"
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val article = new Article("this is title", "hi,this is content")
+    println(article)
+  }
+
 }
